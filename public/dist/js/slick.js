6fe234197b686f67d80bc0cc627ecdbd72bcccfa
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2022-03-04 20:05:14 +0000

    Section Gallery added

--- public/dist/js/slick.js
+++ public/dist/js/slick.js
@@ -1,42 +1,23 @@
+$(".gallery-container").slick({
+    infinite: true,
+    autoplay: true,
+    dots: true,
+    slidesToScroll: 1,
+    slidesToShow: 2,
+    pauseOnDotsHover: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-circle-chevron-left"></i></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-circle-chevron-right"></i></button>',
 
-  
-  // ===== service slider =======
-  $('.gallery-slider').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      prevArrow: '<span class="arrow prev_arrow"><i class="fas fa-angle-left"></i></span>',
-      nextArrow: '<span class="arrow next_arrow"><i class="fas fa-angle-right"></i></span>',
-      autoplaySpeed: 2000,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+    responsive: [
+     
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
+      }
 
-      ]
+    ]
+    
   });
-  
-  
