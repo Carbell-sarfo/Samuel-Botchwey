29e31b47bc3228ee0325c14f563f76731e0fde02
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2022-04-05 17:07:47 +0100

    Content updated

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ const navList = document.querySelector(".nav-list");
 const navSocial = document.querySelector(".nav_social-list");
 const navOverlay = document.querySelector(".nav-overlay");
 const navBackground = document.querySelector(".nav-bg");
-const darkSwitch = document.querySelector(".switch-box");
+// const darkSwitch = document.querySelector(".switch-box");
 const navitem = document.querySelectorAll(".nav-item");
 
 let showNav = false;
@@ -26,7 +26,7 @@ function openNav () {
         navSocial.classList.add("show");
         navOverlay.classList.add("show");
         navBackground.classList.add("show");
-        darkSwitch.classList.add("show");
+        // darkSwitch.classList.add("show");
         navitem.forEach(item =>{
             item.classList.add("show");
         });
@@ -39,7 +39,7 @@ function openNav () {
         navImage.classList.remove("show");
         navSocial.classList.remove("show");
         navOverlay.classList.remove("show");
-        darkSwitch.classList.remove("show");
+        // darkSwitch.classList.remove("show");
         navBackground.classList.remove("show");
         navitem.forEach(item =>{
             item.classList.remove("show");
@@ -53,32 +53,32 @@ function openNav () {
 
 
 // =========== DAY AND NIGHT SWITCH =============
-let darkMode = localStorage.getItem("darkMode");
-const darkModeSwitch = document.querySelector("#switch");
+// let darkMode = localStorage.getItem("darkMode");
+// const darkModeSwitch = document.querySelector("#switch");
 
 // Check if darkmode is enabled
 // if it is disabled, turn it on
 // if it is enabled, turn it off
 
-const enableDarkMode = () => {
-    document.body.classList.add("night");
-    localStorage.setItem('darkMode', "enabled");
-}
-const disableDarkMode = () => {
-    document.body.classList.remove("night");
-    localStorage.setItem('darkMode', null);
-}
+// const enableDarkMode = () => {
+//     document.body.classList.add("night");
+//     localStorage.setItem('darkMode', "enabled");
+// }
+// const disableDarkMode = () => {
+//     document.body.classList.remove("night");
+//     localStorage.setItem('darkMode', null);
+// }
 
-if(darkMode === "enabled") {
-    enableDarkMode();
-    darkModeSwitch.checked = true;
-}
+// if(darkMode === "enabled") {
+//     enableDarkMode();
+//     darkModeSwitch.checked = true;
+// }
 
-darkModeSwitch.addEventListener("click", () => {
-    darkMode = localStorage.getItem('darkMode');
-    if(darkMode !== "enabled") {
-        enableDarkMode();
-    }else {
-        disableDarkMode();
-    }
-});
+// darkModeSwitch.addEventListener("click", () => {
+//     darkMode = localStorage.getItem('darkMode');
+//     if(darkMode !== "enabled") {
+//         enableDarkMode();
+//     }else {
+//         disableDarkMode();
+//     }
+// });
